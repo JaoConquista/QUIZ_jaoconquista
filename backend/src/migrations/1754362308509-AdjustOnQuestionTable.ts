3dc751fe212b69043d10bea7c734963b7d9a1467
JaoConquista <conquistajoaovictor@gmail.com>
2025-08-05 00:17:18 -0300

    (style): prettier config

--- backend/src/migrations/1754362308509-AdjustOnQuestionTable.ts
+++ backend/src/migrations/1754362308509-AdjustOnQuestionTable.ts
@@ -1,20 +1,27 @@
 import { MigrationInterface, QueryRunner } from "typeorm";
 
 export class AdjustOnQuestionTable1754362308509 implements MigrationInterface {
-    name = 'AdjustOnQuestionTable1754362308509'
+  name = "AdjustOnQuestionTable1754362308509";
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`CREATE TYPE "public"."question_type_enum" AS ENUM('multiple_choice', 'boolean', 'short_answer')`);
-        await queryRunner.query(`ALTER TABLE "question" ADD "type" "public"."question_type_enum" NOT NULL`);
-        await queryRunner.query(`ALTER TABLE "question" ADD "options" jsonb`);
-        await queryRunner.query(`ALTER TABLE "question" ALTER COLUMN "answer" DROP NOT NULL`);
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `CREATE TYPE "public"."question_type_enum" AS ENUM('multiple_choice', 'boolean', 'short_answer')`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "question" ADD "type" "public"."question_type_enum" NOT NULL`,
+    );
+    await queryRunner.query(`ALTER TABLE "question" ADD "options" jsonb`);
+    await queryRunner.query(
+      `ALTER TABLE "question" ALTER COLUMN "answer" DROP NOT NULL`,
+    );
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "question" ALTER COLUMN "answer" SET NOT NULL`);
-        await queryRunner.query(`ALTER TABLE "question" DROP COLUMN "options"`);
-        await queryRunner.query(`ALTER TABLE "question" DROP COLUMN "type"`);
-        await queryRunner.query(`DROP TYPE "public"."question_type_enum"`);
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE "question" ALTER COLUMN "answer" SET NOT NULL`,
+    );
+    await queryRunner.query(`ALTER TABLE "question" DROP COLUMN "options"`);
+    await queryRunner.query(`ALTER TABLE "question" DROP COLUMN "type"`);
+    await queryRunner.query(`DROP TYPE "public"."question_type_enum"`);
+  }
 }
